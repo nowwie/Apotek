f724a018bfb73c1e4af030cc2d9b9e77e7ab4bdf
nowwie <novitazkaa@gmail.com>
2025-06-08 23:09:12 +0700

    Co-authored-by: AS <annitapuspitasuci@gmail.com> Co-authored-by: farichao <farichao@users.noreply.github.com> Co-authored-by: Nabil Sahsada Suratno <nabilsahsadacode@gmail.com>

---
+++ js/layout.js
@@ -0,0 +1,14 @@
+// js/layout.js
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("../components/header.html")
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById("header").innerHTML = data;
+    });
+
+  fetch("../components/footer.html")
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById("footer").innerHTML = data;
+    });
+});
